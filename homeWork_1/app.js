d792ab5d58e3f7eedc6add60ae585d4379dad260
vkozub <vsevolod.kozub@gmail.com>
2023-11-03 11:28:51 -0400

    first commit

---
+++ homeWork_1/app.js
@@ -0,0 +1,24 @@
+// task 2
+console.log('Kozub');
+
+// task 3
+let dateNow = new Date();
+let grettings = 'Hi, I am ' + 'Vsevolod Kozub';
+alert(`${dateNow} ${grettings}`);
+[dateNow, grettings] = [grettings, dateNow];
+alert(`${dateNow} ${grettings}`);
+
+// task 4
+let properties = {
+    "String": 'I am a String data type',
+    "Number": 1_000_000,
+    "Boolean": false,
+    "Undefined": undefined,
+    "Null": null
+};
+
+// task 5
+let isAdult = confirm('Are you more than 18 years old?');
+console.log(isAdult);
+
+// task 6
