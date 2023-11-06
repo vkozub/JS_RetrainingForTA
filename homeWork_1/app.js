32696c7410c5d5e5ec67da3bb67949275fa895d3
vkozub <vsevolod.kozub@gmail.com>
2023-11-06 09:20:12 -0500

    task 8 completed

--- homeWork_1/app.js
+++ homeWork_1/app.js
@@ -4,7 +4,7 @@ console.log('Kozub');
 // task 3
 let dateNow = new Date();
 let grettings = 'Hi, I am ' + 'Vsevolod Kozub';
-let message = () => `${dateNow} ${grettings}`;
+let message = () => `${dateNow} ${grettings}`; // using function with closure to avoid code duplication
 alert(message());
 [dateNow, grettings] = [grettings, dateNow];
 alert(message());
@@ -23,3 +23,28 @@ let isAdult = confirm('Are you more than 18 years old?');
 console.log(isAdult);
 
 // task 6
+let myFirtLastGroupInfo = 'Vsevolod Kozub JavaScript for TA [Q4, 2023]';
+let myYearDob = 1982;
+let myMaritalStatus = false;
+let myBikes = null;
+let myCars;
+let myFirtLastGroupInfoType = typeof(myFirtLastGroupInfo);
+let myYearDobType = typeof(myYearDob);
+let myMaritalStatusType = typeof(myMaritalStatus);
+console.log(myYearDob);
+console.log(myMaritalStatus);
+console.log(myFirtLastGroupInfo);
+console.log('My bikes variable type is: ' + typeof(myBikes));
+console.log('My cars variable type is: ' + typeof(myCars));
+
+// task 7
+let userLogin = prompt('Please, enter you login');
+let userEmail = prompt('Please, enter you email');
+let userPassword = prompt('Please, enter you password');
+alert(`Dear ${userLogin}, your email is ${userEmail}, your password is ${userPassword}.`);
+
+//task 8
+let secondsInHour = (hours) => hours * 3600;
+let secondsInDay = (days) => secondsInHour(days * 24);
+let secondsInMonth = (months) => secondsInDay(months * 30); // let assume that we have 30 days in a month
+alert(`Seconds in an hour: ${secondsInHour(1)}, seconds in a day: ${secondsInDay(1)}, seconds in a month: ${secondsInMonth(1)}.`);
