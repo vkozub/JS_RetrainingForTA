a889572a658e647802a799fbc1af2ef90dfdf979
vkozub <vsevolod.kozub@gmail.com>
2023-11-10 08:33:51 -0500

    task 3 completed

--- homeWork_2/app.js
+++ homeWork_2/app.js
@@ -18,3 +18,19 @@ console.log(res4);
 console.log(typeof res4);
 
 // task 2
+
+
+// task 3
+let array = new Array();
+array[0] = Math.random();
+array.push('myString');
+array.push(!!array[0]);
+array[3] = null;
+document.body.innerHTML = `<p>Length of the array is ${array.length}. The array is: ${array}</>`;
+let userValue = prompt("Enter the fifth element into the array!");
+array.push(userValue);
+document.body.innerHTML += `<h2>The 5th element is: ${userValue}</>`;
+array.shift()
+document.body.innerHTML += `<i>${array}</>`;
+
+//task 4
