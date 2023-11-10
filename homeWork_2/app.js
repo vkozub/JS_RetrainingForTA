f45499df68f92f06646cdbe9ec982d6f98f82352
vkozub <vsevolod.kozub@gmail.com>
2023-11-10 07:54:58 -0500

    task 1 completed

--- homeWork_2/app.js
+++ homeWork_2/app.js
@@ -1,1 +1,20 @@
+// task 1
+let x = 1;
+let y = 2;
+let res1 = "" + x + y;
+console.log(res1);
+console.log(typeof res1);
 
+let res2 = 'true' + x*y;
+console.log(res2);
+console.log(typeof res2);
+
+let res3 = !!(y/x);
+console.log(res3);
+console.log(typeof res3);
+
+let res4 = Math.sqrt(-x*y);
+console.log(res4);
+console.log(typeof res4);
+
+// task 2
