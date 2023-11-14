3e926d91eb05bc1ce6afb7e39547f218f84f575c
vkozub <vsevolod.kozub@gmail.com>
2023-11-14 05:28:00 -0500

    task 1 completed

--- homeWork_3/app.js
+++ homeWork_3/app.js
@@ -1,2 +1,12 @@
 // task 1
+const numbers = [2, 3, 4, 5];
+for (let i = 0; i < numbers.length; i++ ) {
+    console.log(numbers[i], i);
+};
+let index = 0
+while (index < numbers.length) {
+    console.log(numbers[index], index);
+    index++;
+};
 
+//task 2
