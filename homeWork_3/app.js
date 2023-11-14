71cb0914727235a3db0b609c009e237e14c80bb2
vkozub <vsevolod.kozub@gmail.com>
2023-11-14 05:41:41 -0500

    task 2 completed

--- homeWork_3/app.js
+++ homeWork_3/app.js
@@ -9,4 +9,10 @@ while (index < numbers.length) {
     index++;
 };
 
-//task 2
+// task 2
+for (let i = 0; i < 15; i++) {
+    let message = (i%2 === 0) ? `${i} is even` : `${i} is odd`;
+    console.log(message)
+}
+
+// task 3
