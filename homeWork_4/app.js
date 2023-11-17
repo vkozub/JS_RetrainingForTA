b64cea14f1f19f7c4156d2c1f692335598595508
vkozub <vsevolod.kozub@gmail.com>
2023-11-17 08:10:00 -0500

    task 1 completed

--- homeWork_4/app.js
+++ homeWork_4/app.js
@@ -1,2 +1,14 @@
 // task 1
+function calcRectangleArea(width, height) {
+    try {
+        if (isNaN(width) || isNaN(height)) { throw new TypeError("Width and height must be a number"); };
+        return Math.abs(width * height).toPrecision();
+    } catch (e) {
+        console.log(e);
+    };
+}
+let width = parseFloat(prompt('Enter the width of rectangle'));
+let height = parseFloat(prompt('Enter the height of rectangle'));
+console.log(calcRectangleArea(width, height));
 
+// task 2
