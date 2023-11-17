545fd15a075ce7aeba0f0ca2086770d493da71b2
vkozub <vsevolod.kozub@gmail.com>
2023-11-17 09:01:05 -0500

    task 2 completed

--- homeWork_4/app.js
+++ homeWork_4/app.js
@@ -12,3 +12,23 @@ let height = parseFloat(prompt('Enter the height of rectangle'));
 console.log(calcRectangleArea(width, height));
 
 // task 2
+let age = prompt('Enter you age');
+function checkAge(age) {
+    try {
+        if (!age) { 
+            throw new RangeError("Empty field! Please enter your age.");
+        } else if (isNaN(parseInt(age))) { 
+            throw new TypeError("Age must be a number");
+        } else if (age < 14) {
+            throw new RangeError("Age must be more than 14 years old");
+        } else {
+            alert('You have got the access to watch the movie');
+        };
+    } catch (e) {
+        console.log(e.name + ' ' + e.message);
+        alert(e.message);
+    };
+}
+checkAge(age);
+
+// task 3
