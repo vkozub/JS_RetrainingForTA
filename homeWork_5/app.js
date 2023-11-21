cad3c22a48989704695db6b889a031470c5bb0c7
vkozub <vsevolod.kozub@gmail.com>
2023-11-21 11:20:47 -0500

    tsak 3 completed

--- homeWork_5/app.js
+++ homeWork_5/app.js
@@ -20,6 +20,43 @@ function showProps(obj) {
 console.log(showProps(car));
 
 // **************************** Task 3 ****************************
+class Person {
+    constructor(name, surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    showFullName() {
+        return `${this.name} ${this.surname}`;
+    }
+}
+class Student extends Person {
+    constructor(name, surname, admYear) {
+        super(name, surname);
+        this.showCourse = admYear;
+    }
+    showFullName(middleName) {
+        return `${this.name} ${middleName} ${this.surname}`;
+    }
+    get showCourse() {
+        return this._admYear;
+    }
+    set showCourse(year) {
+        const nowYear = (new Date).getFullYear();
+        try {
+            if ((nowYear - year + 1) > 6) { // verify year whether more than 6 years ago
+                throw new Error('You are not the student anymore');
+            } else if (year > nowYear) { // verify year whether less than current
+                throw new Error('You are not the student yet');
+            } else { this._admYear = (nowYear - year + 1); }
+        } catch(e) { 
+            console.log(e.message);
+            this._admYear = null; // setting null value in case invalid data
+        };  
+    }
+}
+const student = new Student('Vsevolod', 'Kozub', 2023);
+console.log(student.showFullName('Anatoliyovych'));
+console.log('Current course: ' + student.showCourse);
 
 // **************************** Task 4 ****************************
 
