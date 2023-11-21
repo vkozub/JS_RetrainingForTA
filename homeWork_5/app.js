a74a2625e94635b1851ef71efcf994b11a692efa
vkozub <vsevolod.kozub@gmail.com>
2023-11-21 09:18:26 -0500

    task 1 completed

--- homeWork_5/app.js
+++ homeWork_5/app.js
@@ -1,9 +1,20 @@
-// task 1
+// **************************** Task 1 ****************************
+function propsCount(obj) {
+    return Object.keys(obj).length;
+}
+let car = {
+    speed: 180,
+    color: 'blue',
+    wheels: 4,
+    doors: 4,
+    seats: 5,
+    price: 1000000
+};
+console.log(propsCount(car));
 
+// **************************** Task 2 ****************************
 
-// task 2
+// **************************** Task 3 ****************************
 
-// task 3
-
-// task 4
+// **************************** Task 4 ****************************
 
