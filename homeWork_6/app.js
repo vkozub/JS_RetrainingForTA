7c9988c360abeb9bc6ce448075fda2dd50a1b4e8
vkozub <vsevolod.kozub@gmail.com>
2023-11-27 05:55:52 -0500

    task 3 completed

--- homeWork_6/app.js
+++ homeWork_6/app.js
@@ -12,11 +12,18 @@ let divs = document.getElementsByTagName('div');
 divs[0].innerHTML = 'Last';
 
 // **************************** Task 2 ****************************
+
 let image = document.querySelector('.image');
 image.setAttribute('src', 'cat.jpg');
 alert(image.outerHTML);
 
 // **************************** Task 3 ****************************
+
+const paragraphs = document.querySelectorAll('div#text > p');
+const parArray = Array.from(paragraphs); // nodeList to array
+for ( let i = 0; i < parArray.length; i++ ) {
+    console.log('Selector text ' + `${i}: ` + parArray[i].textContent);
+}
 
 // **************************** Task 4 ****************************
 
