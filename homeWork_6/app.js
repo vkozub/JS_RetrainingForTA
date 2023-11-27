8d0174e194553c05b09b3dad229f2cb17c9ac1bd
vkozub <vsevolod.kozub@gmail.com>
2023-11-27 07:47:06 -0500

    task 4 completed

--- homeWork_6/app.js
+++ homeWork_6/app.js
@@ -25,5 +25,22 @@ parArray.forEach((value, i) => { console.log('Selector text ' + `${i}: ` + value
 
 // **************************** Task 4 ****************************
 
+// the first way
+const uList = document.querySelectorAll('ul#list > li');
+const ul = Array.from(uList);
+const l = ul.length;
+alert(`${ul[0].textContent}, ${ul[l-1].textContent}, ${ul[1].textContent}, ${ul[l-2].textContent}, ${ul[2].textContent}`)
+
+// the second way
+const uListNext = document.querySelectorAll('ul#list > li');
+const ul2 = Array.from(uListNext);
+ul2.forEach((value, i) => { value.setAttribute('id', `list_${i}`); });
+const l0 = document.getElementById('list_0').textContent;
+const l1 = document.getElementById('list_1').textContent;
+const l2 = document.getElementById('list_2').textContent;
+const l3 = document.getElementById('list_3').textContent;
+const l4 = document.getElementById('list_4').textContent;
+alert(l0 + ', ' + l4 + ', ' + l1 + ', ' + l3 + ', ' + l2);
+
 // **************************** Task 5 ****************************
 
