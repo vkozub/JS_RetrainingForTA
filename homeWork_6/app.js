8d723f129d40fc74dab24baaa71808eca1cc9638
vkozub <vsevolod.kozub@gmail.com>
2023-11-27 05:22:29 -0500

    task 1, 2 copmpleted

--- homeWork_6/app.js
+++ homeWork_6/app.js
@@ -1,6 +1,20 @@
 // **************************** Task 1 ****************************
 
+let test = document.getElementById('test');
+test.innerHTML = 'Last';
+
+// the second way
+let test2 = document.querySelector('#test');
+test2.innerHTML = 'Last';
+
+// the third way
+let divs = document.getElementsByTagName('div');
+divs[0].innerHTML = 'Last';
+
 // **************************** Task 2 ****************************
+let image = document.querySelector('.image');
+image.setAttribute('src', 'cat.jpg');
+alert(image.outerHTML);
 
 // **************************** Task 3 ****************************
 
