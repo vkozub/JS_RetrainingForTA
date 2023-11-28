9fbb7d2fa88b45dbef02a8acaa1250d02d0f4e36
vkozub <vsevolod.kozub@gmail.com>
2023-11-28 09:52:08 -0500

    task 7_1 completed

--- homework_7/app.js
+++ homework_7/app.js
@@ -1,3 +1,14 @@
 // ********************************** Task 1 ********************************
+
+let newWin = window.open('', '', "width=300, height=300");
+function resizeNew(win) { win.resizeTo(500, 500); };
+setTimeout(resizeNew, 2000, newWin);
+setTimeout(() => { newWin.moveTo(200, 200); }, 4000);
+setTimeout(() => { newWin.close(); }, 6000);
+
 // ********************************** Task 2 ********************************
+
+
+
 // ********************************** Task 3 ********************************
+
