05f856af97e33261839943ff2a640746dcc1b7b5
vkozub <vsevolod.kozub@gmail.com>
2023-12-03 13:52:10 +0200

    task 8_2 completed

--- homeWork_8/app.js
+++ homeWork_8/app.js
@@ -8,6 +8,17 @@ console.log(z);
 
 // ********************************** Task 2 ********************************
 
+let data = {
+    names: ["Sam", "Tom", "Ray", "Bob"],
+    ages: [20, 24, 22, 26]
+};
+let { names: [, name2, , name4], ages: [, age2, , age4] } = data;
+console.log(name2);
+console.log(age2);
+console.log(name4);
+console.log(age4);
+
+// ********************************** Task 3 ********************************
 
 // ********************************** Task 3 ********************************
 
