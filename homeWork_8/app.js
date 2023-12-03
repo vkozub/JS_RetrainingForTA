3d2387b717ba674ae7cc1d445a4232631c1be59b
vkozub <vsevolod.kozub@gmail.com>
2023-12-03 15:33:25 +0200

    task 8_4 start

--- homeWork_8/app.js
+++ homeWork_8/app.js
@@ -36,5 +36,30 @@ console.log(mul(null, "str", false, true));
 
 // ********************************** Task 4 ********************************
 
+let server = {
+    data: 0,
+    convertToString: function(callback) {
+        callback(( function () {
+            return this.data + "";
+        }).bind(this));
+    }
+};
+let client = {
+    server: server,
+    result: '',
+    calc: function(data) {
+        this.server.data = data;
+        this.server.convertToString(this.notification());
+    },
+    notification: function() {
+        return ( function(callback) {
+            this.result = callback();
+        }).bind(this);
+    }
+};
+
+client.calc(123);
+console.log(client.result);
+console.log(typeof client.result);
 
 // ********************************** Task 5 ********************************
