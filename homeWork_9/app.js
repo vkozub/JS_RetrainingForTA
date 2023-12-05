0f9689087942ec4088b6ac87f3c58db365a9dd9c
vkozub <vsevolod.kozub@gmail.com>
2023-12-05 10:21:50 -0500

    task 9_2 completed

--- homeWork_9/app.js
+++ homeWork_9/app.js
@@ -10,10 +10,21 @@ function getPromise(message, interval) {
 
 getPromise("test promise", 2000).then(result => console.log(result));
 
-
 // ********************************** Task 2 ********************************
 
+function calcArrProduct(arr){
+    return new Promise((resolve) => {
+        let product = 1;
+        for (let i in arr) {
+            if (!(typeof arr[i] === 'number')){ product = 'Error! Incorrect array!'; break;
+        } else { product *= arr[i] };
+        };
+        resolve(product);
+    });
+}
 
+calcArrProduct([3, 4, 5]).then(result => console.log(result));
+calcArrProduct([5, "user2", 7, 12]).then(result => console.log(result));
 
 // ********************************** Task 3 ********************************
 
