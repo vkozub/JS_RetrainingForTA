ff2ef3b49f7e5bcf75316b601cb3a084dd8eed7b
vkozub <vsevolod.kozub@gmail.com>
2023-12-11 05:47:11 -0500

    task 10_1 completed

---
+++ homeWork_10/server.js
@@ -0,0 +1,19 @@
+// ********************************** Task 1 ********************************
+
+const http = require('http');
+const os = require('os');
+const path = require('path');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(`<h1>System information</h1>`);
+    res.write(`<h3>Current user name: ${os.userInfo().username}</h3>`);
+    res.write(`<h3>OS type: ${os.platform()}</h3>`);
+    res.write(`<h3>System work time: ${(os.uptime()/60).toFixed(2)} minutes</h3>`);
+    res.write(`<h3>Current working directory: ${path.dirname(__filename)}</h3>`);
+    res.write(`<h3>Server file name: ${path.basename(__filename)}</h3>`);
+    res.end();
+}).listen(5000);
+
+// ********************************** Task 2 ********************************
+
