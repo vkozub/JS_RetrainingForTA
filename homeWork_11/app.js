3c8d1a747d922f1a712253a2e231cee54dc97384
vkozub <vsevolod.kozub@gmail.com>
2023-12-13 07:34:46 -0500

    update task 1

--- homeWork_11/app.js
+++ homeWork_11/app.js
@@ -1,13 +1,14 @@
 // ********************************** Task 1 ********************************
 
+const voteButton = document.getElementById('voteButton');
+
 async function vote() {
-const url = 'http://localhost:5000';
-const response = await fetch(url);
-const data = await response.json();
-console.log(data);
+    const url = 'http://localhost:5000/voting';
+    const response = await fetch(url);
+    const data = await response.json();
+    voteButton.innerHTML = 'Your vote is accepted: ' + data.date;
 }
 
-const voteButton = document.getElementById('voteButton');
 voteButton.addEventListener('click', vote);
 
 // ********************************** Task 2 ********************************
