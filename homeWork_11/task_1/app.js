40e4e3e25c0781719ae24cd77fdd88cba3642433
vkozub <vsevolod.kozub@gmail.com>
2023-12-15 05:37:14 -0500

    task 11_1 completed

--- homeWork_11/task_1/app.js
+++ homeWork_11/task_1/app.js
@@ -2,14 +2,11 @@
 
 const voteButton = document.getElementById('voteButton');
 
-async function vote() {
+async function vote(event) {
     const url = 'http://localhost:5000/voting';
     const response = await fetch(url);
     const data = await response.json();
-    voteButton.innerHTML = 'Your vote is accepted: ' + data.date;
+    event.target.innerHTML = 'Your vote is accepted: ' + data.date;
 }
 
-voteButton.addEventListener('click', vote);
-
-// ********************************** Task 2 ********************************
-
+voteButton.addEventListener('click', (e) => vote(e));
