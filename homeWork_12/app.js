ea594fe811586ba1416bd814ad9631df6111c4a3
vkozub <vsevolod.kozub@gmail.com>
2023-12-18 07:10:05 -0500

    Task HW 12_2 completed

--- homeWork_12/app.js
+++ homeWork_12/app.js
@@ -12,7 +12,14 @@ upperCase('RegExp');
 
 // ********************************** Task 2 ********************************
 
+// add dot (part before @ sign) in pattern because Gmail allows it, for DSN domains only lowercase letters
 
+function checkEmail(email) {
+    const upCasePattern = /^\w+\.?\w+\@[a-z]+\.[a-z]+$/g;
+    return upCasePattern.test(email);
+}
+
+console.log(checkEmail('vsevolod.kozub@gmail.com'));
 
 // ********************************** Task 3 ********************************
 
