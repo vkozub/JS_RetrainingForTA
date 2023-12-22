878cd0ce2f3a151531e3bf109bcd2c53faff1e35
vkozub <vsevolod.kozub@gmail.com>
2023-12-22 09:55:44 -0500

    update 14_1

--- homeWork_14/test/task1.js
+++ homeWork_14/test/task1.js
@@ -17,8 +17,9 @@ describe('Task 1', function() {
     it('Check that the 2nd element of the list is active', async () => {
         await driver.get('https://the-internet.herokuapp.com/dropdown');
         await driver.findElement(By.id('dropdown')).click();
-        let secondOption = await driver.findElements(By.xpath('//select[@id="dropdown"]/option'))[1].isEnabled();
-        assert.ok(secondOption, 'The second option of dropdown is disabled');
+        let optionsArray = await driver.findElements(By.xpath('//select[@id="dropdown"]/option'));
+        let secondOptionActive = await optionsArray[1].isEnabled();
+        assert.ok(secondOptionActive, 'The second option of dropdown is disabled');
 
     });
 
