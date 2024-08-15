e39b764c4c0097de906fa06dc6678546957e564a
vkozub <vsevolod.kozub@gmail.com>
2024-08-15 10:06:10 -0400

    added reslove() to the Promises

--- working-with-fs-module-async-callback.js
+++ working-with-fs-module-async-callback.js
@@ -26,33 +26,66 @@ const { Buffer } = require('node:buffer');
     console.log('File path to write: ', filePath);
     const fileData = 'Write new data to the new file.\n';
 
-    async function writeFile(path, data) {
-        return new Promise((reject) => {
-            fs.writeFile(path, data, function (err) {
+    async function writeNewFile(path, data) {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(path, data, err => {
                 if (err) reject(err);
-                console.log(`File ${path} written to ${baseDir} successfully.`);
+                console.log(`File ${path} written to '${baseDir}' successfully.`);
+                resolve();
             });
         });
     }
 
     try {
-        await writeFile(filePath, fileData);
+        await writeNewFile(filePath, fileData);
     } catch (err) {
         console.error('File was not written! Error is: ', err.message);
     }
     
     // Truncate the file
-    try {
-        fs.access(filePath, fs.constants.W_OK | fs.constants.R_OK, err => {
-            if (err) throw(err);
-            console.log('File is accessible.');
+    async function accessFile(path) {
+        return new Promise((resolve, reject) => {
+            fs.access(path, fs.constants.F_OK, function (err) {
+                if (err) reject(err);
+                console.log('File is accessible.');
+                resolve();
+            });
         });
-        fs.truncate(filePath, err => {
-            if (err) throw(err);
-            console.log('File hase been truncated.');
-        });
-    } catch (err) {
-        console.log('File has not been truncated. Error is: ', err);
     }
+
+    async function truncateFile(path) {
+        return new Promise((resolve, reject) => {
+            fs.truncate(path, function (err) {
+                if (err) reject(err);
+                console.log('File hase been truncated.');
+                resolve();
+            });
+        });
+    }
+
+    async function truncateAccessibleFile(path) {
+        try {
+            await accessFile(path);
+            await truncateFile(path);
+        } catch (err) {
+            console.log('File has not been truncated. Error is: ', err);
+        }
+    }
+    
+    await truncateAccessibleFile(filePath);
+
+    // Append file
+    const fileDataAppend = 'Append data to the new file.\n';
+    async function appendAccessibleFile(path, data) {
+        return new Promise((resolve, reject) => {
+            fs.appendFile(path, data, function (err) {
+                if (err) reject(err);
+                console.log(`File ${path} appended successfully.`);
+                resolve();
+            });
+        });
+    }
+
+    await appendAccessibleFile(filePath, fileDataAppend);
 
 })('test/dir1');
